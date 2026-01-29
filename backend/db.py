c4504c53c171bbf507d88c1e07ea0711849f2ea2
Om <omthote24@gmail.com>
2026-01-30 00:58:00 +0530

    Update database password in db.py

--- backend/db.py
+++ backend/db.py
@@ -3,7 +3,7 @@ import mysql.connector
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="password",   # change if needed
+    password="12345",   
     database="photo_album"
 )
 
